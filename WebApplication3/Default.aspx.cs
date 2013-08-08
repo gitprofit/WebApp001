6a4671045c1846ab5af588a1b3c8ed0ddc8d6e2c
gitprofit <liszcz.michal@gmail.com>
2013-08-08 15:59:49 +0200

    q

--- WebApplication3/Default.aspx.cs
+++ WebApplication3/Default.aspx.cs
@@ -5,10 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Oracle.DataAccess.Client;
+
 namespace WebApplication3
 {
 	public partial class Default : System.Web.UI.Page
 	{
+		private OracleConnection connection;
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			if (!IsPostBack) InitPage();
@@ -18,7 +22,15 @@ namespace WebApplication3
 
 		protected void InitPage()
 		{
-			//Connection conn = new SqlC
+			var para = new Util.OracleConnectionParams();
+
+			para.Host = "st-2013-130.staz.comarch";
+			para.Port = "1521";
+			para.SID = "orcl";
+			para.Username = "APEX_PUBLIC_USER";
+			para.Password = "Root123!";
+
+			connection = new OracleConnection(para.ConnectionString);
 		}
 	}
 }
